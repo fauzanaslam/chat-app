fae82ba55b611bf781eae81d8d22b8d61eb91db3
fauzanaslam <fauzanaslamdhani@gmail.com>
2025-05-20 14:32:15 +0700

    change url

--- server/index.js
+++ server/index.js
@@ -17,10 +17,9 @@ const server = createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: ["https://chat-app-blush-rho-98.vercel.app"],
         methods: ["GET", "POST"],
-        credentials: true,
-    },
+    }
 });
 
 io.on("connection", (socket) => {
@@ -62,23 +61,6 @@ io.on("connection", (socket) => {
             });
         }
     });
-});
-
-io.on('connection', (socket) => {
-    console.log(`User Connected: ${socket.id}`);
-
-    // socket.on('join_room', (data) => {
-    //     socket.join(data);
-    //     console.log(`User with ID: ${socket.id} joined room: ${data}`);
-    // });
-
-    // socket.on('send_message', (data) => {
-    //     socket.to(data.room).emit('receive_message', data);
-    // });
-
-    // socket.on('disconnect', () => {
-    //     console.log('User Disconnected', socket.id);
-    // });
 });
 
 const port = process.env.PORT || 3001;
